c635a9c95272cd73f7b1e4fe63dbde441e090984
Akash Mondal <a98mondal@gmail.com>
2025-05-31 22:58:17 +0000

    add umami tracker to the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {process.env.UMAMI_SCRIPT_URL && <script defer src={process.env.UMAMI_SCRIPT_URL} data-website-id={process.env.UMAMI_DATA_WEBSITE_ID}></script>}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
